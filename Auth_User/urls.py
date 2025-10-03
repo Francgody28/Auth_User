ba5342263982f5a43cc857d71b44488159e4f034
Francgody28 <francgody693@gmail.com>
2025-10-03 14:50:45 +0300

    Ten Commit

--- Auth_User/urls.py
+++ Auth_User/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Authentication.urls')),  # Include Authentication app routes at root
+    path('', include('Authentication.urls')),  # Including Authentication app routes at root
 ]
